d8c14c833b4bf19501c28e2987ed04e689a07050
yan.chenasz <Yan.Chenasz@advantech.com.cn>
2020-12-23 18:21:49 +0800

    encoding config

--- src/main/java/top/dustone/bilibili/terminal/App.java
+++ src/main/java/top/dustone/bilibili/terminal/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Not a helloworld");
-        System.out.println( "Hello World!" );
+        System.out.println("ababaababababa");
+        System.out.println("ababaaba你好");
+        System.out.println( "\u2593\u2593\u2593" );
     }
 }
