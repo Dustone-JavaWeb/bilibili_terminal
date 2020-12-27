78a7313c2f823e46db221b9f8bc3c44088f366dd
dustone <13048062053@163.com>
2020-12-27 23:42:55 +0800

    config model

--- src/main/java/top/dustone/terminal/lasagna/model/PaperContainer.java
+++ src/main/java/top/dustone/terminal/lasagna/model/PaperContainer.java
@@ -7,16 +7,32 @@ import lombok.Data;
  * @Email: 13048062053@163.com
  * @Group: Dustone Network Corp
  * @Date: 2020/12/25 11:32
- *
- *
- *
+ * <p>
+ * <p>
+ * The container of panels
  */
 @Data
-public class PaperContainer extends PaperRoot implements Comparable{
-    int pageLevel;
+public class PaperContainer extends PaperRoot implements Comparable {
+    /**
+     * The default value of paper level,
+     */
+    public static int DEFAULT_PAPER_LEVEL = 0;
+    int paperLevel = DEFAULT_PAPER_LEVEL;
+
 
     @Override
     public int compareTo(Object o) {
-        return 0;
+        if (o instanceof PaperContainer && o != null) {
+            PaperContainer p = (PaperContainer) o;
+            if (p.paperLevel > this.paperLevel) {
+                return -1;
+            } else if(p.paperLevel < this.paperLevel){
+                return 1;
+            }else{
+                return 0;
+            }
+        } else {
+            return -1;
+        }
     }
 }
